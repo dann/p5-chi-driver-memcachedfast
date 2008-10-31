74bf9abdb933c7a394eb4dd54554a187b84b7148
Dann <techmemo@gmail.com>
2008-11-01 02:59:02 +0900

    initial commit

---
+++ lib/CHI/Driver/MemcachedFast.pm
@@ -0,0 +1,33 @@
+package CHI::Driver::MemcachedFast;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+CHI::Driver::MemcachedFast -
+
+=head1 SYNOPSIS
+
+  use CHI::Driver::MemcachedFast;
+
+=head1 DESCRIPTION
+
+CHI::Driver::MemcachedFast is
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
